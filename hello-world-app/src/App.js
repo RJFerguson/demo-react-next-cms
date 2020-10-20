e30cdae58dac2050ed98189305f7757626f6adff
Ryan Ferguson <ryan_ferguson@mckinsey.com>
2020-10-20 17:07:04 -0400

    initial context

--- hello-world-app/src/App.js
+++ hello-world-app/src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "halfmoon/css/halfmoon-variables.min.css";
+import * as halfmoon from "halfmoon";
 
-function App() {
+export default function App() {
+  useEffect(() => {
+    halfmoon.onDOMContentLoaded();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="page-wrapper with-navbar">
+      <div className="sticky-alerts"></div>
+      <nav className="navbar">
+        <span className="navbar-text text-monospace">v1.0</span>
+        <button
+          className="btn btn-action ml-10"
+          type="button"
+          onClick={() => halfmoon.toggleDarkMode()}
+          aria-label="Toggle dark mode"
         >
-          Learn React
-        </a>
-      </header>
+          <i className="fa fa-adjust" aria-hidden="true"></i>
+        </button>
+        <ul className="navbar-nav d-none d-md-flex ml-auto">
+          <li className="nav-item">
+            <div href="#" className="nav-link">
+              Docs
+            </div>
+          </li>
+          <li className="nav-item">
+            <div href="#" className="nav-link">
+              Questions
+            </div>
+          </li>
+          <li className="nav-item">
+            <div href="#" className="nav-link">
+              Sign In
+            </div>
+          </li>
+          <li className="nav-item">
+            <div href="#" className="nav-link">
+              Register
+            </div>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="content-wrapper">
+        <div className="container-md">
+          <ul></ul>
+
+          <div className="card">
+            <h2 className="card-title">Card title</h2>
+            ...
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
